1c41382fcbc11e13207324aab6fbacbd132b61ce
Jamie Maclean <jamie.maclean@numberica.com>
2024-12-17 15:19:12 +0100

    update release version

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,29 +6,29 @@ import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 
 const links = {
-  version: '0.1.9',
+  version: '0.1.10',
   notes: 'See the assets to download this version and install.',
   pub_date: '2024-12-12T13:06:36.275Z',
   platforms: {
     'darwin-aarch64': {
       signature:
         'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVSMVlzaWRYYklxVkVHWW16bExTRk9FTTVRNWpxWmNFU05qRlMxQWwyc3lKR3BWcHZXaDZrbll3Qk03RXlxcmRvVFJlL1prYjFVWDVaOWgveEpVdEdpSlpydTFjeHkxaUFvPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzM0MDA4MTMwCWZpbGU6T3BlbkZpcmUuYXBwLnRhci5negppTHFTdERxSk1rQWFWRnR2c1lwc3J6a0VQN3pGbXRMQkRPTXlsa04rN3NWSkJGdkltQzZjNmsxSytDT2F4dmVaUGR1aDZZTlpsRGpLSmEwR3k0WEJCQT09Cg==',
-      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.9/OpenFire_aarch64.app.tar.gz',
+      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.10/OpenFire_aarch64.app.tar.gz',
     },
     'darwin-x86_64': {
       signature:
         'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVSMVlzaWRYYklxVkNNdnFqOUZzNVkvWmpPamZUVU9vR3lVZ2taK3p6dEpvaEZqdng2V2hoVk9tdGgwYnFrMTFvYkk2UmwvOTZ5cHg5TndFdGZnbHlRSmUxa0Z6UFp6dEE4PQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzM0MDA4MTQ5CWZpbGU6T3BlbkZpcmUuYXBwLnRhci5nego0VUU2QzJjOFZER0FrMUZIc0liMUZxdjkrcXdXS3ZHY2ptQUYycXMzL0drenpQcVlVdDZkY2JXZ1pkaEVVM1lTdkhRWXAvT1c0NmRsd3JrdGl5MElCZz09Cg==',
-      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.9/OpenFire_x64.app.tar.gz',
+      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.10/OpenFire_x64.app.tar.gz',
     },
     'linux-x86_64': {
       signature:
         'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVSMVlzaWRYYklxVk9XcGxPQkc4bmdFN01BdkdqTE5wWkxJUXQwbDk1TjJnb2NrLzZFOW9acUhQUUdUT1BMOEwvUmZkVGg0UlNxcGFKVXR3K21NRDdPdnBPVUxscExUWFE4PQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzM0MDA4MzI0CWZpbGU6T3BlbkZpcmVfMC4xLjlfYW1kNjQuQXBwSW1hZ2UKZEdrUlphZHZnMjBjUi9FMTZGSU55bXZnNEo4YTRJTXFLOGhPY2NaRnExc2F6eW9WZ3lGVVJhNzlTOE9hSVdSdGNuM1A3ejdvcHJXc1NmU1h6dGkwQlE9PQo=',
-      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.9/OpenFire_0.1.9_amd64.AppImage',
+      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.10/OpenFire_0.1.10_amd64.AppImage',
     },
     'windows-x86_64': {
       signature:
         'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVSMVlzaWRYYklxVkh5WTgzancwenVTK0x5ZzdmWGVVbjEwUnJUYVBJOTN0RjFSaTViOE1WQk00aHNhU2ZaSGhrL2xVZGR1N0ZMY0dwaEdrNmlySTRadmZObmM2OUlFRUFrPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzM0MDA4NzkzCWZpbGU6T3BlbkZpcmVfMC4xLjlfeDY0X2VuLVVTLm1zaQp1cXUxNmlwK0gxOXRyVi9oSjZFK053TGM3ZUlXSGs5SEJBblBoNWRHVHpkMGxTSHJVVXZBRFlLMUtmdUxSYUQ4TUtrd2dTT1dBS0hKL1k3b2FEUlhEUT09Cg==',
-      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.9/OpenFire_0.1.9_x64_en-US.msi',
+      url: 'https://github.com/fire-library/openfire/releases/download/openfire-v0.1.10/OpenFire_0.1.10_x64_en-US.msi',
     },
   },
 }
